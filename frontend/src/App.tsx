2a3e6728c21db53667e02eb07f62556130201c32
robertorincos <beto.rinco.silveira@gmail.com>
2026-02-21 22:10:49 -0300

    Add GamePage v1

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "@/routes/Home";
 import Emulators from "@/routes/Emulators";
 import About from "./routes/About";
+import GameOverview from "./routes/GameOverview";
 
 function App() {
   const appName = import.meta.env.VITE_APPLICATION_NAME || "Myrient Search";
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home appName={appName} />} />
         <Route path="/emulators" element={<Emulators appName={appName} />} />
         <Route path="/about" element={<About appName={appName} />} />
+        <Route path="/game/:id" element={<GameOverview appName={appName} />} />
       </Routes>
     </BrowserRouter>
   );
